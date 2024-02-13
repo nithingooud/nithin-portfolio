612093f13db12dbdbb2605871697994831cbf309
Nithin <goudnithin77@gmail.com>
2024-02-13 08:08:14 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,15 +21,19 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4 text-white fixed w-full z-10 top-0" style={{ cursor: 'default' }}>
       <div className="mx-auto text-center flex w-5/6 justify-between">
+        
+        <div className="hidden sm:flex space-x-4 items-center text-sm">
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+        </div>
         <div className="text-3xl sm:text-2xl font-extrabold">
           <a href="/">Nithin Manda</a>
         </div>
-        <div className="hidden sm:flex space-x-4 items-center text-lg">
-          <Link to='/'>Home</Link>
-          <Link to='/about'>About</Link>
+        <div className="hidden sm:flex space-x-4 items-center text-sm">
           <Link to='/experience'>Experience</Link>
           <Link to='/contact'>Contact</Link>
         </div>
+
         <div className="sm:hidden">
           <button onClick={toggleMobileMenu} className="text-xl focus:outline-none">
             {isMobileMenuOpen ? '✕' : '☰'}

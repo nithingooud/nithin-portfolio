0ee79f4121c8bcd8da04bd31605790bbec446c82
Nithin <nithin@inncircles.com>
2024-07-26 12:18:03 +0530

    added email passkey to env

--- src/constants.js
+++ src/constants.js
@@ -45,6 +45,6 @@ export const experiences = [
 
 
 
-export const EMAIL_JS_SERVICE_ID = 'service_90wb9t5';
-export const EMAIL_JS_TEMPLATE_ID = 'template_8l87rew';
-export const EMAIL_JS_PUBLIC_KEY = 'pHPjc9RGGkF66r86N';
+export const EMAIL_JS_SERVICE_ID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
+export const EMAIL_JS_TEMPLATE_ID = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
+export const EMAIL_JS_PUBLIC_KEY = process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY;
